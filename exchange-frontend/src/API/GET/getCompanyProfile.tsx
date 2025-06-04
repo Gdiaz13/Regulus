8bd75467646ed6907c67c8cda04d241639a09690
German Diaz <gdiaz1092@gmail.com>
2025-06-04 16:59:24 -0400

    added a new api to be able to get both price and a make detailed outlook and each stock

---
+++ exchange-frontend/src/API/GET/getCompanyProfile.tsx
@@ -0,0 +1,19 @@
+import { type ICompanyProfile } from "../../Interfaces/ICompanyProfile"
+import axios from "axios"
+
+export const getCompanyProfile = async  (query: string) => {
+    try {
+        const data = await axios.get<ICompanyProfile[]>(
+            `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${import.meta.env.VITE_EXCHANGE_KEY}`
+        )
+        return data
+    } catch (error: any) {
+        if (error.isAxiosError) {
+            console.log('Error message: ', error.message)
+            return error.message
+        } else {
+            console.log('Unexpected error: ', error.message)
+            return error.message
+        }
+    }
+}
--- exchange-frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ exchange-frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import type { ICompanyProfile } from '../../Interfaces/ICompanyProfile';
+import { getCompanyProfile } from '../../API/GET/getCompanyProfile';
+import styles from './CompanyPage.module.css';
 
-interface Props {}
+const CompanyPage = () => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<ICompanyProfile>();
 
-const CompanyPage = (props: Props) => {
+  useEffect(() => {
+    const getProfileInfo = async () => {
+      try {
+        const result = await getCompanyProfile(ticker!);
+        console.log('Company profile API result:', result);
+        setCompany(result?.data[0]);
+      } catch (error) {
+        console.log('Error fetching company profile:', error);
+      }
+    };
+    getProfileInfo();
+    }, []);
+
   return (
-    <div>CompanyPage</div>
+    <div className={styles.companyContainer}>
+      {company ? (
+        <div>
+          <img className={styles.companyLogo} src={company.image} alt={`${company.companyName} logo`} />
+          <h1 className={styles.companyHeader}>{company.companyName} ({company.symbol})</h1>
+          <p className={styles.companyPrice}>Price: ${company.price}</p>
+          <p className={styles.companyInfo}>Exchange: {company.exchange}</p>
+          <p className={styles.companyInfo}>Industry: {company.industry}</p>
+          <p className={styles.companyDesc}>Description: {company.description}</p>
+        </div>
+      ) : (
+        <p>Loading company information...</p>
+      )}
+    </div>
   )
 }
 
-export default CompanyPage
+export default CompanyPage;
